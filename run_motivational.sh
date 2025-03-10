e4043057e281a67f7fa1a4beaa65fb68a83786a7
Philipp Raith <p.raith@dsg.tuwien.ac.at>
2025-03-10 10:46:23 +0100

    update

---
+++ run_motivational.sh
@@ -0,0 +1,11 @@
+python -m src.motivational.reactiveandproactiveparalleldiffworkloads R1-1233-output 1 ./data/nofs-ids/workload-configs/R1-1233-7500-20.json 1 4 R1 1233
+python -m src.motivational.reactiveandproactiveparalleldiffworkloads R1-1358-output 1 ./data/nofs-ids/workload-configs/R1-1358-7500-20.json 1 4 R1 1358
+python -m src.motivational.reactiveandproactiveparalleldiffworkloads R1-1412-output 1 ./data/nofs-ids/workload-configs/R1-1412-7500-20.json 1 4 R1 1412
+
+python -m src.motivational.reactiveandproactiveparalleldiffworkloads R1-1437-output 1 ./data/nofs-ids/workload-configs/R1-1437-7500-20.json 1 4 R1 1437
+python -m src.motivational.reactiveandproactiveparalleldiffworkloads R1-1465-output 1 ./data/nofs-ids/workload-configs/R1-1465-7500-20.json 1 4 R1 1465
+python -m src.motivational.reactiveandproactiveparalleldiffworkloads R1-2119-output 1 ./data/nofs-ids/workload-configs/R1-2119-7500-20.json 1 4 R1 2119
+
+python -m src.motivational.reactiveandproactiveparalleldiffworkloads R1-351-output 1 ./data/nofs-ids/workload-configs/R1-351-7500-20.json 1 4 R1 351
+python -m src.motivational.reactiveandproactiveparalleldiffworkloads R1-49-output 1 ./data/nofs-ids/workload-configs/R1-49-7500-20.json 1 4 R1 49
+python -m src.motivational.reactiveandproactiveparalleldiffworkloads R1-817-output 1 ./data/nofs-ids/workload-configs/R1-817-7500-20.json 1 4 R1 817
